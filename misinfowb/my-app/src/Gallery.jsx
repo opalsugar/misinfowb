425e5656a9ba76d685514b295696eb012c573a8b
opalsugar <reneexsingh@gmail.com>
2024-11-20 22:48:56 -0800

    Current progress

--- misinfowb/my-app/src/Gallery.jsx
+++ misinfowb/my-app/src/Gallery.jsx
@@ -4,9 +4,26 @@ import { gameData } from './data';
 
 export default function Gallery() {
   const [search, setSearch] = useState('');
+  const [formatFilter, setFormatFilter] = useState([]);
 
   const handleSearchInput = (e) => {
     setSearch(e.target.value)
+  };
+
+  const handleFormat = (e) => {
+    let isSelected = e.target.checked;
+    let value = e.target.value;
+
+    console.log(isSelected);
+    if (isSelected) {
+      setFormatFilter([...formatFilter, value]);
+    } else {
+      setFormatFilter((formatFilter) => {
+        return formatFilter.filter((tag) => {
+          return tag !== e.target.value
+        })
+      })
+    }
   };
 
   return (
@@ -21,8 +38,8 @@ export default function Gallery() {
           <div className="filter-item">
             <h5>Format:</h5>
             <form>
-              <p><label><input type="checkbox" className="filter" />Online</label></p>
-              <p><label><input type="checkbox" className="filter" />Physical</label></p>
+              <p><label><input checked={formatFilter.includes("Online")} type="checkbox" className="filter" value="Online" onChange={handleFormat} />Online</label></p>
+              <p><label><input checked={formatFilter.includes("Physical")} type="checkbox" className="filter" value="Physical" onChange={handleFormat} />Physical</label></p>
             </form>
           </div>
           <div className="filter-item">
@@ -57,24 +74,27 @@ export default function Gallery() {
       <div class="gallery-container">
         {gameData.filter((data) => {
           return data.title.toLowerCase().includes(search.toLowerCase());
-        }).map((item) => {
-          return (
-            <div className="gallery-item">
-              <div className="gallery-image">
-                <img src={item.img} alt="Game" />
-              </div>
-              <a href="./game1.html">
-                <h1>{item.title}</h1>
-              </a>
-              <p>{item.desc}</p>
-              <p>Format: {item.format}</p>
-              <p>Genre: {item.genre}</p>
-              <p>Type: {item.type}</p>
-              <p>Target players: {item['Target players']}</p>
-            </div>
-          )
+        }).filter((data) => {
+        })
 
-        })}
+          .map((item) => {
+            return (
+              <div className="gallery-item">
+                <div className="gallery-image">
+                  <img src={item.img} alt="Game" />
+                </div>
+                <a href="./game1.html">
+                  <h1>{item.title}</h1>
+                </a>
+                <p>{item.desc}</p>
+                <p>Format: {item.format}</p>
+                <p>Genre: {item.genre}</p>
+                <p>Type: {item.type}</p>
+                <p>Target players: {item['Target players']}</p>
+              </div>
+            )
+
+          })}
       </div>
     </>
   );
--- misinfowb/my-app/src/data.jsx
+++ misinfowb/my-app/src/data.jsx
@@ -2,7 +2,7 @@ export const gameData = [
     {
         'title': 'Euphorigen Investigation',
         'desc': 'A digital media literacy game where players work together to investigate misleading information and uncover the truth behind Euphorigen, a fictional substance. Players collaborate as journalists to verify facts and identify misinformation.',
-        'format': 'Online, Physical',
+        'format': ['Online, Physical'],
         'genre': 'Education, Puzzle, Role-playing, Simulation',
         'type': 'Group',
         'Target players': 'Everyone',
@@ -11,7 +11,7 @@ export const gameData = [
     {
         'title': 'The Galaxy Escape Room: Mission',
         'desc': 'Galaxy Escape Room is a space-themed puzzle adventure where players must solve a series of challenging puzzles and decipher clues to escape from a futuristic spaceship. Set against the backdrop of outer space, players work against the clock to repair systems, unlock hidden mysteries, and navigate through various rooms to complete their mission before time runs out.',
-        'format': 'Online, Physical',
+        'format': ['Online, Physical'],
         'genre': 'Puzzle, Role-playing, Adventure',
         'type': 'Group',
         'Target players': 'Everyone',
@@ -20,7 +20,7 @@ export const gameData = [
     {
         'title': 'Libertas Veritas: Freedom and Truth',
         'desc': 'Libertas Veritas: Freedom and Truth is a thought-provoking game where players navigate complex political and social environments to uncover hidden truths while preserving freedom of speech. Through decision-making and investigative tasks, players explore themes of liberty, justice, and misinformation in a dynamic, story-driven setting.',
-        'format': 'Online',
+        'format': ['Online'],
         'genre': 'Simulation, Social, Education',
         'type': 'Individual',
         'Target players': 'Everyone',
@@ -29,7 +29,7 @@ export const gameData = [
     {
         'title': 'Bad News',
         'desc': 'Bad News is a misinformation game designed to teach players about the tactics used to spread fake news, including emotional manipulation and disinformation strategies. Players take on the role of a fake news creator and learn how to spot fake news by creating it.',
-        'format': 'Online',
+        'format': ['Online'],
         'genre': 'Simulation, Social, Quiz',
         'type': 'Individual',
         'Target players': 'Everyone',
@@ -38,7 +38,7 @@ export const gameData = [
     {
         'title': 'Breaking Harmony Square',
         'desc': 'Breaking Harmony square is a misinformation game which teaches players about the creation and spread of misinformation online. The game has players post misinformation to get likes, teaching them how misinformation is made and spread, and what some of its goals are.',
-        'format': 'Online',
+        'format': ['Online'],
         'genre': 'Simulation, Social, Education',
         'type': 'Individual',
         'Target players': 'Everyone',
@@ -47,7 +47,7 @@ export const gameData = [
     {
         'title': 'Fakey',
         'desc': 'Fakey is a media literacy game where players interact with a simulated social media feed, deciding whether to like, share, or fact-check articles and posts. The goal is to train players to recognize misinformation, fake news, and clickbait by challenging them to assess the credibility of content. Through these choices, players learn to improve their critical thinking skills and become more discerning consumers of information online, all while navigating the fast-paced environment of a typical news feed.',
-        'format': 'Online',
+        'format': ['Online'],
         'genre': 'Quiz, Education, Simulation',
         'type': 'Individual',
         'Target players': 'Everyone',
