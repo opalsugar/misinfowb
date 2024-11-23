5c1432b6f89629cdd8783ebeabd6419946cb5220
opalsugar <reneexsingh@gmail.com>
2024-11-22 16:19:36 -0800

    Search and filter functional

--- misinfowb/my-app/src/Gallery.jsx
+++ misinfowb/my-app/src/Gallery.jsx
@@ -5,6 +5,9 @@ import { gameData } from './data';
 export default function Gallery() {
   const [search, setSearch] = useState('');
   const [formatFilter, setFormatFilter] = useState([]);
+  const [targetFilter, setTargetFilter] = useState([]);
+  const [typeFilter, setTypeFilter] = useState([]);
+  const [genreFilter, setGenreFilter] = useState([]);
 
   const handleSearchInput = (e) => {
     setSearch(e.target.value)
@@ -14,12 +17,56 @@ export default function Gallery() {
     let isSelected = e.target.checked;
     let value = e.target.value;
 
-    console.log(isSelected);
     if (isSelected) {
       setFormatFilter([...formatFilter, value]);
     } else {
       setFormatFilter((formatFilter) => {
         return formatFilter.filter((tag) => {
+          return tag !== e.target.value
+        })
+      })
+    }
+  };
+
+  const handleTarget = (e) => {
+    let isSelected = e.target.checked;
+    let value = e.target.value;
+
+    if (isSelected) {
+      setTargetFilter([...targetFilter, value]);
+    } else {
+      setTargetFilter((targetFilter) => {
+        return targetFilter.filter((tag) => {
+          return tag !== e.target.value
+        })
+      })
+    }
+  };
+
+  const handleType = (e) => {
+    let isSelected = e.target.checked;
+    let value = e.target.value;
+
+    if (isSelected) {
+      setTypeFilter([...typeFilter, value]);
+    } else {
+      setTypeFilter((typeFilter) => {
+        return typeFilter.filter((tag) => {
+          return tag !== e.target.value
+        })
+      })
+    }
+  };
+
+  const handleGenre = (e) => {
+    let isSelected = e.target.checked;
+    let value = e.target.value;
+
+    if (isSelected) {
+      setGenreFilter([...genreFilter, value]);
+    } else {
+      setGenreFilter((genreFilter) => {
+        return genreFilter.filter((tag) => {
           return tag !== e.target.value
         })
       })
@@ -38,63 +85,68 @@ export default function Gallery() {
           <div className="filter-item">
             <h5>Format:</h5>
             <form>
-              <p><label><input checked={formatFilter.includes("Online")} type="checkbox" className="filter" value="Online" onChange={handleFormat} />Online</label></p>
-              <p><label><input checked={formatFilter.includes("Physical")} type="checkbox" className="filter" value="Physical" onChange={handleFormat} />Physical</label></p>
+              <p><label><input type="checkbox" className="filter" value="Online" checked={formatFilter.includes("Online")} onChange={handleFormat} />Online</label></p>
+              <p><label><input type="checkbox" className="filter" value="Physical" checked={formatFilter.includes("Physical")} onChange={handleFormat} />Physical</label></p>
             </form>
           </div>
           <div className="filter-item">
             <h5>Target Players:</h5>
             <form>
-              <p><label><input type="checkbox" className="filter" />Elementary</label></p>
-              <p><label><input type="checkbox" className="filter" />Middle School</label></p>
-              <p><label><input type="checkbox" className="filter" />High School</label></p>
-              <p><label><input type="checkbox" className="filter" />Tertiary Education</label></p>
+              <p><label><input type="checkbox" className="filter" value="Elementary" checked={targetFilter.includes("Elementary")} onChange={handleTarget} />Elementary</label></p>
+              <p><label><input type="checkbox" className="filter" value="Middle School" checked={targetFilter.includes("Middle School")} onChange={handleTarget} />Middle School</label></p>
+              <p><label><input type="checkbox" className="filter" value="High School" checked={targetFilter.includes("High School")} onChange={handleTarget} />High School</label></p>
+              <p><label><input type="checkbox" className="filter" value="Everyone" checked={targetFilter.includes("Everyone")} onChange={handleTarget} />Everyone</label></p>
             </form>
           </div>
           <div className="filter-item">
             <h5>Type:</h5>
             <form>
-              <p><label><input type="checkbox" className="filter" />Single-player</label></p>
-              <p><label><input type="checkbox" className="filter" />Multi-player</label></p>
-              <p><label><input type="checkbox" className="filter" />Online</label></p>
-              <p><label><input type="checkbox" className="filter" />Virtual</label></p>
+              <p><label><input type="checkbox" className="filter" value="Single-player" checked={typeFilter.includes("Single-player")} onChange={handleType} />Single-player</label></p>
+              <p><label><input type="checkbox" className="filter" value="Multi-player" checked={typeFilter.includes("Multi-player")} onChange={handleType} />Multi-player</label></p>
+              <p><label><input type="checkbox" className="filter" value="Online" checked={typeFilter.includes("Online")} onChange={handleType} />Online</label></p>
+              <p><label><input type="checkbox" className="filter" value="Virtual" checked={typeFilter.includes("Virtual")} onChange={handleType} />Virtual</label></p>
             </form>
           </div>
           <div className="filter-item">
             <h5>Genre:</h5>
             <form>
-              <p><label><input type="checkbox" className="filter" />Action</label></p>
-              <p><label><input type="checkbox" className="filter" />Puzzle</label></p>
-              <p><label><input type="checkbox" className="filter" />Roleplaying</label></p>
-              <p><label><input type="checkbox" className="filter" />Simulation</label></p>
+              <p><label><input type="checkbox" className="filter" value="Action" checked={genreFilter.includes("Action")} onChange={handleGenre} />Action</label></p>
+              <p><label><input type="checkbox" className="filter" value="Puzzle" checked={genreFilter.includes("Puzzle")} onChange={handleGenre} />Puzzle</label></p>
+              <p><label><input type="checkbox" className="filter" value="Roleplaying" checked={genreFilter.includes("Roleplaying")} onChange={handleGenre} />Roleplaying</label></p>
+              <p><label><input type="checkbox" className="filter" value="Simulation" checked={genreFilter.includes("Simulation")} onChange={handleGenre} />Simulation</label></p>
             </form>
           </div>
         </div>
       </div>
       <div class="gallery-container">
-        {gameData.filter((data) => {
-          return data.title.toLowerCase().includes(search.toLowerCase());
-        }).filter((data) => {
-        })
-
-          .map((item) => {
-            return (
-              <div className="gallery-item">
-                <div className="gallery-image">
-                  <img src={item.img} alt="Game" />
-                </div>
-                <a href="./game1.html">
-                  <h1>{item.title}</h1>
-                </a>
-                <p>{item.desc}</p>
-                <p>Format: {item.format}</p>
-                <p>Genre: {item.genre}</p>
-                <p>Type: {item.type}</p>
-                <p>Target players: {item['Target players']}</p>
+        {gameData.filter((item) => {
+          return item.title.toLowerCase().includes(search.toLowerCase());
+        }).filter((item) => {
+          return formatFilter.every((tag) => item.format.includes(tag));
+        }).filter((item) => {
+          return targetFilter.every((tag) => item['Target players'].includes(tag));
+        }).filter((item) => {
+          return typeFilter.every((tag) => item.type.includes(tag));
+        }).filter((item) => {
+          return genreFilter.every((tag) => item.genre.includes(tag));
+        }).map((item) => {
+          return (
+            <div className="gallery-item">
+              <div className="gallery-image">
+                <img src={item.img} alt="Game" />
               </div>
-            )
+              <a href="./game1.html">
+                <h1>{item.title}</h1>
+              </a>
+              <p>{item.desc}</p>
+              <p>Format: {item.format.toString()}</p>
+              <p>Genre: {item.genre.toString()}</p>
+              <p>Type: {item.type.toString()}</p>
+              <p>Target players: {item['Target players'].toString()}</p>
+            </div>
+          )
 
-          })}
+        })}
       </div>
     </>
   );
--- misinfowb/my-app/src/data.jsx
+++ misinfowb/my-app/src/data.jsx
@@ -2,55 +2,55 @@ export const gameData = [
     {
         'title': 'Euphorigen Investigation',
         'desc': 'A digital media literacy game where players work together to investigate misleading information and uncover the truth behind Euphorigen, a fictional substance. Players collaborate as journalists to verify facts and identify misinformation.',
-        'format': ['Online, Physical'],
-        'genre': 'Education, Puzzle, Role-playing, Simulation',
-        'type': 'Group',
-        'Target players': 'Everyone',
+        'format': ['Online', 'Physical'],
+        'genre': ['Education', 'Puzzle', 'Role-playing', 'Simulation'],
+        'type': ['Multi-player'],
+        'Target players': ['Everyone'],
         'img': './img/euphorigen.jpg'
     },
     {
         'title': 'The Galaxy Escape Room: Mission',
         'desc': 'Galaxy Escape Room is a space-themed puzzle adventure where players must solve a series of challenging puzzles and decipher clues to escape from a futuristic spaceship. Set against the backdrop of outer space, players work against the clock to repair systems, unlock hidden mysteries, and navigate through various rooms to complete their mission before time runs out.',
-        'format': ['Online, Physical'],
-        'genre': 'Puzzle, Role-playing, Adventure',
-        'type': 'Group',
-        'Target players': 'Everyone',
+        'format': ['Online', 'Physical'],
+        'genre': ['Puzzle', 'Role-playing', 'Adventure'],
+        'type': ['Multi-player'],
+        'Target players': ['Everyone'],
         'img': './img/galaxy.png'
     },
     {
         'title': 'Libertas Veritas: Freedom and Truth',
         'desc': 'Libertas Veritas: Freedom and Truth is a thought-provoking game where players navigate complex political and social environments to uncover hidden truths while preserving freedom of speech. Through decision-making and investigative tasks, players explore themes of liberty, justice, and misinformation in a dynamic, story-driven setting.',
         'format': ['Online'],
-        'genre': 'Simulation, Social, Education',
-        'type': 'Individual',
-        'Target players': 'Everyone',
+        'genre': ['Simulation', 'Social', 'Education'],
+        'type': ['Single-player'],
+        'Target players': ['Everyone'],
         'img': './img/libertas.png'
     },
     {
         'title': 'Bad News',
         'desc': 'Bad News is a misinformation game designed to teach players about the tactics used to spread fake news, including emotional manipulation and disinformation strategies. Players take on the role of a fake news creator and learn how to spot fake news by creating it.',
         'format': ['Online'],
-        'genre': 'Simulation, Social, Quiz',
-        'type': 'Individual',
-        'Target players': 'Everyone',
+        'genre': ['Simulation', 'Social', 'Quiz'],
+        'type': ['Single-player'],
+        'Target players': ['Everyone'],
         'img': './img/badnews.png'
     },
     {
         'title': 'Breaking Harmony Square',
         'desc': 'Breaking Harmony square is a misinformation game which teaches players about the creation and spread of misinformation online. The game has players post misinformation to get likes, teaching them how misinformation is made and spread, and what some of its goals are.',
         'format': ['Online'],
-        'genre': 'Simulation, Social, Education',
-        'type': 'Individual',
-        'Target players': 'Everyone',
+        'genre': ['Simulation', 'Social', 'Education'],
+        'type': ['Single-player'],
+        'Target players': ['Everyone'],
         'img': './img/harmonysq.png'
     },
     {
         'title': 'Fakey',
         'desc': 'Fakey is a media literacy game where players interact with a simulated social media feed, deciding whether to like, share, or fact-check articles and posts. The goal is to train players to recognize misinformation, fake news, and clickbait by challenging them to assess the credibility of content. Through these choices, players learn to improve their critical thinking skills and become more discerning consumers of information online, all while navigating the fast-paced environment of a typical news feed.',
         'format': ['Online'],
-        'genre': 'Quiz, Education, Simulation',
-        'type': 'Individual',
-        'Target players': 'Everyone',
+        'genre': ['Quiz', 'Education', 'Simulation'],
+        'type': ['Single-player'],
+        'Target players': ['Everye'],
         'img': './img/fakey.png'
     }
 ]
